452eb2c7cda8a70e05a9da062215db1e199419a0
Jeferson Lopes <59580761+jeferson1opes@users.noreply.github.com>
2022-08-22 09:53:56 -0300

    fix formik

--- src/modules/Forms/RegisterForm.jsx
+++ src/modules/Forms/RegisterForm.jsx
@@ -24,61 +24,63 @@ export default function RegisterForm({ viewLogin }) {
   };
 
   return (
-    <Formik
-      initialValues={{ username: "", name: "", password: "" }}
-      onSubmit={handleSubmit}
-    >
-      {({ values, handleChange, handleSubmit }) => (
-        <form onSubmit={handleSubmit} className="w-25 p-3">
-          <TextField
-            margin="normal"
-            fullWidth
-            name="username"
-            required
-            id="outlined-required"
-            label="UserName"
-            value={values.username}
-            type="text"
-            onChange={handleChange}
-          />
+    <>
+      <Formik
+        initialValues={{ username: "", name: "", password: "" }}
+        onSubmit={handleSubmit}
+      >
+        {({ values, handleChange, handleSubmit }) => (
+          <form onSubmit={handleSubmit} className="w-25 p-3">
+            <TextField
+              margin="normal"
+              fullWidth
+              name="username"
+              required
+              id="outlined-required"
+              label="UserName"
+              value={values.username}
+              type="text"
+              onChange={handleChange}
+            />
 
-          <TextField
-            margin="normal"
-            fullWidth
-            required
-            name="name"
-            id="outlined-required"
-            label="Name"
-            value={values.name}
-            onChange={handleChange}
-          />
-          <TextField
-            margin="normal"
-            fullWidth
-            required
-            id="outlined-required"
-            label="Password"
-            type="password"
-            name="password"
-            value={values.password}
-            onChange={handleChange}
-          />
-          <ButtonCustom
-            variant="contained"
-            loading={loading}
-            type="submit"
-            text="Register"
-          />
-          <ButtonCustom
-            style={{ marginTop: "10px", width: "100%" }}
-            variant="outlined"
-            onClick={viewLogin}
-            type="submit"
-            text="Already have an account?"
-          />
-        </form>
-      )}
+            <TextField
+              margin="normal"
+              fullWidth
+              required
+              name="name"
+              id="outlined-required"
+              label="Name"
+              value={values.name}
+              onChange={handleChange}
+            />
+            <TextField
+              margin="normal"
+              fullWidth
+              required
+              id="outlined-required"
+              label="Password"
+              type="password"
+              name="password"
+              value={values.password}
+              onChange={handleChange}
+            />
+            <ButtonCustom
+              variant="contained"
+              loading={loading}
+              type="submit"
+              text="Register"
+            />
+            <ButtonCustom
+              style={{ marginTop: "10px", width: "100%" }}
+              variant="outlined"
+              onClick={viewLogin}
+              type="submit"
+              text="Already have an account?"
+            />
+          </form>
+        )}
+      </Formik>
       <ToastContainerCustom />
-    </Formik>
+    </>
   );
 }
